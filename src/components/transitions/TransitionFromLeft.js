6eb02f648649124f8f85f9222e70d78e104e547b
Christian Catalan <crcatala@gmail.com>
2018-10-25 23:46:40 -0500

    Transitions

--- src/components/transitions/TransitionFromLeft.js
+++ src/components/transitions/TransitionFromLeft.js
@@ -4,20 +4,21 @@ import { TweenMax } from "gsap/src/uncompressed/TweenMax";
 
 export default function TransitionFromLeft(props) {
   const offset = 100;
+  const durationInSeconds = 0.25;
 
   return (
     <Transition
-      timeout={1000}
+      timeout={durationInSeconds * 1000 + 1}
       appear
       mountOnEnter
       unmountOnExit
       in={props.in}
       onEnter={(node, isAppearing) => {
-        TweenMax.to(node, 0, { x: offset, autoAlpha: 0 });
-        TweenMax.to(node, 0.5, { x: 0, autoAlpha: 1 });
+        TweenMax.to(node, 0, { x: -offset, autoAlpha: 0 });
+        TweenMax.to(node, durationInSeconds, { x: 0, autoAlpha: 1 });
       }}
       onExiting={(node, isAppearing) => {
-        TweenMax.to(node, 0.5, { x: offset, autoAlpha: 0 });
+        TweenMax.to(node, durationInSeconds, { x: -offset, autoAlpha: 0 });
       }}
     >
       {props.children}
